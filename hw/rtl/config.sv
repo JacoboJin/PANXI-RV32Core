0218c89a24263866dc695c4ba5ec67b77a54a735
Albert <jinzj99@163.com>
2024-05-19 16:37:40 +0800

    Update config.sv

--- hw/rtl/config.sv
+++ hw/rtl/config.sv
@@ -7,21 +7,34 @@
 `ifndef _CONFIG_V_
 `define _CONFIG_V_
 
-`define RISCV
 
-`ifndef PANXI_FPGA_EMUL
-`ifdef SYNTHESIS
-`define ASIC
+`define PANXI_CFG_DEBUG_HAS_JTAG
+`define PANXI_CFG_IRQ_NEED_SYNC
+
+// Define addr_size
+`define PANXI_CFG_ADDR_SIZE_IS_32
+
+`ifdef PANXI_CFG_ADDR_SIZE_IS_16
+    `define PANXI_CFG_ADDR_SIZE   16
 `endif
+`ifdef PANXI_CFG_ADDR_SIZE_IS_32
+    `define PANXI_CFG_ADDR_SIZE   32
+`endif
+`ifdef PANXI_CFG_ADDR_SIZE_IS_24
+    `define PANXI_CFG_ADDR_SIZE   24
 `endif
 
+
+`define PANXI_CFG_REGNUM_IS_32
+
+`define PANXI_CFG_HAS_ITCM
+    // 64KB have address 16bits wide
+    //   The depth is 64*1024*8/64=8192
+`define PANXI_CFG_ITCM_ADDR_WIDTH  16
 
 `define ROM_ADDR_WIDTH      12
 `define ROM_START_ADDR      32'h8000
 
-`ifndef SYNTHESIS
-// `define DATA_STALL_RANDOM
-// `define INSTR_STALL_RANDOM
-`endif
+
 
 `endif
